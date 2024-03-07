1b2e99d904a62acfcd03c6c3260fdc32b739e885
Annazubakha <korepovaanna@gmail.com>
2024-03-07 19:09:12 +0100

    changed path and console.table

--- contacts.js
+++ contacts.js
@@ -3,7 +3,7 @@ const { nanoid } = require("nanoid");
 const fs = require("fs/promises");
 const path = require("path");
 
-const contactsPath = path.join(__dirname, "./db/contacts.json");
+const contactsPath = path.join(__dirname, "db", "contacts.json");
 
 const updateContacts = async (contacts) =>
   await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
